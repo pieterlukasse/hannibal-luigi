5cf5de9f6344cdb90bda1845023af6aad379d1a3
Eliseo Papa <elipapa@users.noreply.github.com>
2017-03-14 14:16:35 +0000

    working ES target, but trouble with parameters

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,8 +1,10 @@
 import unittest
 import logging
 import datetime
+import os
 import luigi
 from luigi.contrib.docker_runner import DockerTask
+from luigi.contrib.esindex import ElasticsearchTarget
 
 from os.path import expanduser
 homedir = expanduser("~")
@@ -12,45 +14,78 @@ logger = logging.getLogger('luigi-interface')
 BRANCH = "latest"
 
 class OpenTargETLTask(DockerTask):
+
+    # find which ES to point to. For now we save the status and the data
+    # in the same cluster
+    eshost = luigi.configuration.get_config().get('elasticsearch',
+                                                        'eshost', '127.0.0.1')
+    esport = luigi.configuration.get_config().get('elasticsearch',
+                                                 'esport', '9200')
+    
+    # read from the config file how to call the marker index, where
+    # to store the status of each task.
+    marker_index = luigi.configuration.get_config().get('elasticsearch',
+                                                        'marker-index', 'update_log')
+    marker_doc_type = luigi.configuration.get_config().get('elasticsearch',
+                                                           'marker-doc-type', 'entry')
+
     image = ':'.join(["eu.gcr.io/open-targets/data_pipeline", BRANCH])
+    volumes=[os.getcwd() + '/data:/tmp/data']
+    network_mode='host'
     environment = {
-        "ELASTICSEARCH_HOST":"127.0.0.1",
-        "ELASTICSEARCH_PORT":"9200",
+        "ELASTICSEARCH_HOST": eshost,
+        "ELASTICSEARCH_PORT": esport,
         "CTTV_DUMP_FOLDER":"/tmp/data",
         "CTTV_DATA_VERSION":"test-17.02"
     }
 
+    run_options = luigi.Parameter(default='-h')
 
-class HelpOptions(OpenTargETLTask):
-    '''
-    Just print an help message. Useful step only to figure out if anything is wrong with the container.
-    TODO move to tests
-    '''
-    command = ['python', 'run.py', '--do-nothing']
-    volumes=['/Users/epapa/ot/OpenTargETL/opentargets-luigi-test.txt:/usr/src/app/output.log']
+    @property
+    def command(self):
+        return ['python','run.py',run_options]
+
+    def output(self):
+        """
+        Returns a ElasticsearchTarget representing the inserted dataset.
+        """
+        return ElasticsearchTarget(
+            host=self.eshost,
+            port=self.esport,
+            index=self.marker_index,
+            doc_type=self.marker_doc_type,
+            update_id=self.task_id
+            )
+
+
+
+    def run(self):
+        '''
+        extend run() of docker runner base class to touch a DB-based target.
+        Opted not to extend the base class, since a docker runner job
+        may prefer to create a local target, which does not implement a touch()
+        method.
+        '''
+        DockerTask.run(self)
+        self.output().touch()
+
+
+
+
+class EFOdata(OpenTargETLTask):
+    auto_remove= False
+    
+    
         
-    
-    def output(self):
-        return luigi.LocalTarget("/Users/epapa/ot/OpenTargETL/opentargets-luigi-test.txt")
 
 
-class LoadBaseData(OpenTargETLTask):
-    '''
-    Load the base data from each main source
-    '''
-
-    command = ['python', 'run.py', '--uni']
-    
-    def requires(self):
-        return []
- 
-    def output(self):
-        return luigi.LocalTarget("test-uni.txt")
-
+class GeneData(OpenTargETLTask):
+    pass
 
 class Validate(OpenTargETLTask):
     '''
-    Run the validation step, which takes the JSON submitted by each provider and makes sure they adhere to our JSON schema
+    Run the validation step, which takes the JSON submitted by each provider
+    and makes sure they adhere to our JSON schema
     '''
 
     command = ['python', 'run.py', '--val', '--remote-file','https://storage.googleapis.com/opentargets-data-sources/16.12/cttv001_gene2phenotype-29-07-2016.json.gz']
