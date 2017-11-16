f502647588cba7876726ca1bb346418efb07819f
Eliseo Papa <elipapa@users.noreply.github.com>
2017-11-16 12:09:40 +0000

    back to big machines

--- launch-gce.sh
+++ launch-gce.sh
@@ -6,14 +6,15 @@ if [ $# -ne 1 ]; then
 fi
 
 DATE=$(date +"%m%d-%H%M")
+CONTAINER_TAG=$1
 ESMACHINE=${2:-elasticsearch}
 echo Will point to ES: $ESMACHINE:9200
 
 
 gcloud beta compute --project "open-targets-eu-dev" instances create "hannibal-$1-$DATE" \
  --zone "europe-west1-d" \
- --machine-type "n1-standard-1" \
  --subnet "default" \
+--machine-type "custom-40-266240" \
  --no-restart-on-failure \
  --maintenance-policy "TERMINATE" \
  --scopes default,storage-rw \
@@ -22,12 +23,14 @@ gcloud beta compute --project "open-targets-eu-dev" instances create "hannibal-$
  --image-family debian-9 \
  --boot-disk-size "250" \
  --boot-disk-type "pd-ssd" \
- --boot-disk-device-name "hannibal-$1-$DATE" \
+ --boot-disk-device-name "hannibal-$CONTAINER_TAG-$DATE" \
  --metadata-from-file startup-script=hannibal-debian.sh \
- --metadata "container-tag=$1","es-url=$ESMACHINE" \
- --labels app=hannibal
-#  --machine-type "custom-40-266240" \
-#  --preemptible \
+ --metadata "container-tag=$CONTAINER_TAG","es-url=$ESMACHINE" \
+ --labels app=hannibal \
+ --preemptible
+
+#  --machine-type "n1-standard-1" \
+
 
 
 
